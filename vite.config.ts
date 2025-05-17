a6b78278a0cdfd6a586559af02ce45ea9ce4cdf1
tjuandev <tpydeveloper@gmail.com>
2025-05-17 17:36:16 -0300

    chore: fix ts forcing casing and add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
@@ -30,7 +31,12 @@ export default defineConfig(({ mode }) => {
       passWithNoTests: true,
       setupFiles: ['./vitest.setup.ts'],
       testTransformMode: { web: ["/\.tsx?$/"] },
-      include: ['src/**/*.test.{ts,tsx}']
+      include: ['src/**/*.test.{ts,tsx}'],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
     }
   }
 })
