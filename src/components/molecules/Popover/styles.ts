38fb8c25ad9d36c8ae12493b794b77e5b186ffd4
tjuandev <tpydeveloper@gmail.com>
2025-05-18 16:56:23 -0300

    feat: add popover component

---
+++ src/components/molecules/Popover/styles.ts
@@ -0,0 +1,15 @@
+import clsx from 'clsx'
+
+export const S = {
+  content: clsx(
+    'z-50',
+    'w-72',
+    'origin-[--radix-popover-content-transform-origin]',
+    'rounded-md',
+    'border',
+    'bg-popover',
+    'p-4',
+    'text-popover-foreground',
+    'shadow-md'
+  )
+}
