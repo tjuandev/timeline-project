7852a2cd0633ade7a0a47673dc5d8e94c603c6b7
tjuandev <tpydeveloper@gmail.com>
2025-05-18 20:39:58 -0300

    chore: adjusted mock to todays date

--- src/pages/Dashboard/mocks.ts
+++ src/pages/Dashboard/mocks.ts
@@ -5,99 +5,99 @@ export const TIMELINE_ITEMS_MOCK: TimelineItem[] = [
   {
     id: 1,
     name: 'First item',
-    start: '2021-01-01',
-    end: '2021-01-05',
+    start: '2025-05-18',
+    end: '2025-05-22',
     color: 'red'
   },
   {
     id: 2,
     name: 'Second item',
-    start: '2021-01-02',
-    end: '2021-01-08',
+    start: '2025-05-19',
+    end: '2025-05-25',
     color: 'yellow'
   },
   {
     id: 3,
     name: 'Another item',
-    start: '2021-01-06',
-    end: '2021-01-13',
+    start: '2025-05-23',
+    end: '2025-05-30',
     color: 'green'
   },
   {
     id: 4,
     name: 'Another item',
-    start: '2021-01-14',
-    end: '2021-01-14',
+    start: '2025-05-31',
+    end: '2025-05-31',
     color: 'blue'
   },
   {
     id: 5,
     name: 'Third item',
-    start: '2021-02-01',
-    end: '2021-02-15',
+    start: '2025-06-18',
+    end: '2025-07-02',
     color: 'purple'
   },
   {
     id: 6,
     name: 'Fourth item with a super long name',
-    start: '2021-01-12',
-    end: '2021-02-16',
+    start: '2025-05-29',
+    end: '2025-07-03',
     color: 'lightBlue'
   },
   {
     id: 7,
     name: 'Fifth item with a super long name',
-    start: '2021-02-01',
-    end: '2021-02-02',
+    start: '2025-06-18',
+    end: '2025-06-19',
     color: 'gray'
   },
   {
     id: 8,
     name: 'First item',
-    start: '2021-01-03',
-    end: '2021-01-05',
+    start: '2025-05-20',
+    end: '2025-05-22',
     color: 'purple'
   },
   {
     id: 9,
     name: 'Second item',
-    start: '2021-01-04',
-    end: '2021-01-08',
+    start: '2025-05-21',
+    end: '2025-05-25',
     color: 'yellow'
   },
   {
     id: 10,
     name: 'Another item',
-    start: '2021-01-06',
-    end: '2021-01-13',
+    start: '2025-05-23',
+    end: '2025-05-30',
     color: 'green'
   },
   {
     id: 11,
     name: 'Another item',
-    start: '2021-01-09',
-    end: '2021-01-09',
+    start: '2025-05-27',
+    end: '2025-05-27',
     color: 'blue'
   },
   {
     id: 12,
     name: 'Third item',
-    start: '2021-02-01',
-    end: '2021-02-15',
+    start: '2025-06-18',
+    end: '2025-07-02',
     color: 'lightBlue'
   },
   {
     id: 13,
     name: 'Fourth item with a super long name',
-    start: '2021-01-12',
-    end: '2021-02-16',
+    start: '2025-05-29',
+    end: '2025-07-03',
     color: 'gray'
   },
   {
     id: 14,
     name: 'Fifth item with a super long name',
-    start: '2021-02-01',
-    end: '2021-02-02',
+    start: '2025-06-18',
+    end: '2025-06-19',
     color: 'purple'
   }
 ]
